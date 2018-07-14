36ed2030c147cffb170ea8a7c7eb5d918921ade6
Toorusr <toorusr@protonmail.com>
2018-07-15 00:56:10 +0200

    Add: free domains to file

--- src/souper.py
+++ src/souper.py
@@ -9,12 +9,15 @@ variants = [a+b+c for a,b,c in product(chars, repeat=3)]
 
 free = []
 
+freedump = open('freedomains.dump', 'w')
+
 def check(x):
     dom = x + ".de"
     details = pythonwhois.get_whois(dom)
     if details['status'][0] == "free":
         print("\r\t\t\t\033[1m\033[92m%s\tFREE\033[0m" % dom)
         free.append(dom)
+        freedump.write(dom + "\n")
     else:
         # print("\t\t\t\033[91m%s\tTAKEN\033[0m" % dom)
         pass
@@ -36,3 +39,4 @@ def numbs():
     for x in variants:
         check(x)
 random()
+freedump.close()
