847ec119b6aec6e5344abffec2d3078fe938f617
Toorusr <toorusr@protonmail.com>
2018-07-15 14:30:15 +0200

    Add: specials parser

--- src/parser.py
+++ src/parser.py
@@ -32,13 +32,29 @@ class Parser:
                 pass
         return array
 
+    def special(parsed):
+        array = []
+        for x in parsed:
+            # print(x[1][0] + " =? " + x[1][2])
+            if x[0] == "FRE":
+                if x[1][0] == x[1][2]:
+                    array.append(x)
+                elif x[1][0] == x[1][1]:
+                    array.append(x)
+                elif x[1][1] == x[1][2]:
+                    array.append(x)
+                else:
+                    pass
+            else:
+                pass
+        return array
 
 
 
-with open("freedomains_150618.dump", "r") as file:
+with open("freedomains.bump", "r") as file:
     lines = file.readlines()
     parsed = Parser(lines, ":").read
-    print(str(Parser.manipul(parsed)))
+    # print(str(Parser.manipul(parsed)))
     freec = 0
     takec = 0
     for x in parsed:
@@ -47,7 +63,10 @@ with open("freedomains_150618.dump", "r") as file:
             print("[o] \033[1m\033[92mFree:\t " + x[1] + "\033[0m")
         elif x[0] == "TAK":
             takec += 1
-            print("[o] \033[1m\033[91mTaken:\t " + x[1] + "\033[0m")
+            # print("[o] \033[1m\033[91mTaken:\t " + x[1] + "\033[0m")
         else:
             pass
     print("\n[i] Process of parsing finised with {} taken and {} free domains. \n [i] Total domains scanned: {}".format(takec, freec, takec + freec))
+    print("[i] Parsing specials (0=2, 0=1, 1=2)")
+    for x in Parser.special(parsed):
+        print("[S] \033[1m\033[92mFree:\t " + x[1] + "\033[0m")
